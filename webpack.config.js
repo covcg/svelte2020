4d66e77f55e6be8e5180994880ab1c1415fe4f6a
covcg <coello_cg95@hotmail.com>
2020-09-06 15:07:44 -0500

    class 14

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['*', '.mjs', '.js', '.svelte']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.svelte$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'svelte-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: path.resolve(__dirname, 'index.html')
+    })
+  ]
+}
