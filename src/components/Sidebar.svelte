ef41cdcd68c83a01e5c3e7b41ab76da1ff1df17a
covcg <coello_cg95@hotmail.com>
2020-09-06 18:36:21 -0500

    class19

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -4,6 +4,16 @@ import Stories from './Stories.svelte';
 import SidebarFooter from './SidebarFooter.svelte';
 </script>
 
+<style>
+  .Sidebar {
+    position: relative;
+    padding: 4.5em 0 0 0;
+  }
+  .Sidebar-container {
+    position: fixed;
+  }
+</style>
+
 <div class="Sidebar">
   <div class="Sidebar-container">
     <Profile />
--- src/components/SidebarFooter.svelte
+++ src/components/SidebarFooter.svelte
@@ -1,3 +1,12 @@
+<style>
+  .SidebarFooter {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
 <div class="SidebarFooter">
   <div class="SidebarFooter-container">
     <p class="SidebarFooter-copy">© 2020 PUGSTAGRAM FROM PLATZI</p>
