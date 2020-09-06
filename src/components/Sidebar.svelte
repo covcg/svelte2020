3eea66a8d208400ada1a0df41f26d8765e7faec3
covcg <coello_cg95@hotmail.com>
2020-09-06 15:48:46 -0500

    class 17 + challenge

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,1 +1,13 @@
-<div class="Sidebar"></div>
+<script>
+import Profile from './Profile.svelte';
+import Stories from './Stories.svelte';
+import SidebarFooter from './SidebarFooter.svelte';
+</script>
+
+<div class="Sidebar">
+  <div class="Sidebar-container">
+    <Profile />
+    <Stories />
+    <SidebarFooter />
+  </div>
+</div>
---
+++ src/components/SidebarFooter.svelte
@@ -0,0 +1,5 @@
+<div class="SidebarFooter">
+  <div class="SidebarFooter-container">
+    <p class="SidebarFooter-copy">© 2020 PUGSTAGRAM FROM PLATZI</p>
+  </div>
+</div>
